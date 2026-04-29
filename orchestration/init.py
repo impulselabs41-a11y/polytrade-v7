b8d402f56abd6c3a3c5bd244236c59a3d1ea59a5
impulselabs41-a11y <impulselabs41@gmail.com>
2026-04-29 16:42:21 +0800

    Create init.py

---
+++ orchestration/init.py
@@ -0,0 +1,10 @@
+from .agency_agents import AgencyDebateSystem, AgentRole, DebateOutcome
+from .task_orchestrator import PolyTradeOrchestrator, SystemState
+
+__all__ = [
+    'AgencyDebateSystem',
+    'AgentRole',
+    'DebateOutcome',
+    'PolyTradeOrchestrator',
+    'SystemState'
+]
